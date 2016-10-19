7bd6c63e637ebc428276ffb9fe1ab9d4ddc777ba
Bill Currie <bill@taniwha.org>
2016-10-19 20:02:40 +0900

    Add a tool to dump the transform hierarchy.

---
+++ hierarchy.py
@@ -0,0 +1,17 @@
+from mu import Mu
+import sys
+
+def check_transform(obj, level):
+    print("    " * level + obj.transform.name)
+
+def check_obj(obj, level = 0):
+    check_transform(obj, level)
+    for o in obj.children:
+        check_obj(o, level + 1)
+
+for fname in sys.argv[1:]:
+    mu = Mu()
+    if not mu.read(fname):
+        print("could not read: " + fname)
+        raise
+    check_obj(mu.obj)
