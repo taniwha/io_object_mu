949602b3ab3f36d5c2d4f3ff7cd567fb88c254e5
Bill Currie <bill@taniwha.org>
2018-02-11 17:21:22 +0900

    Support shader presets.
    Comes with presets for the KSP shaders, though I'm not yet sure how to
    install them. However, user defined presets can be made as well.

---
+++ presets/shaders/ksp_diffuse.py
@@ -0,0 +1,28 @@
+# KSP/Diffuse
+import bpy
+mat = bpy.context.material.mumatprop
+
+mat.name = ''
+mat.shaderName = 'KSP/Diffuse'
+mat.color.name = ''
+mat.color.properties.clear()
+mat.color.index = 0
+mat.color.expanded = False
+mat.vector.name = ''
+mat.vector.properties.clear()
+mat.vector.index = 0
+mat.vector.expanded = False
+mat.float2.name = ''
+mat.float2.properties.clear()
+mat.float2.index = 0
+mat.float2.expanded = False
+mat.float3.name = ''
+mat.float3.properties.clear()
+mat.float3.index = 0
+mat.float3.expanded = False
+item = mat.texture.properties.add()
+item.name = '_MainTex'
+item.tex = 'gray'
+item = mat.color.properties.add()
+item.name = '_Color'
+item.value = (1, 1, 1, 1)
