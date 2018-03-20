2cf22102f1c037dbc33bbfbf02a43f8bdc72116d
Bill Currie <bill@taniwha.org>
2018-03-20 11:32:50 +0900

    Fix a missing comma for 4-d vectors.

--- mucfg.py
+++ mucfg.py
@@ -37,7 +37,7 @@ def add_vector(name, mu, vec, node):
     elif len(vec) == 3:
         node.AddValue(name, "%.9g, %.9g, %.9g" % vec)
     elif len(vec) == 4:
-        node.AddValue(name, "%.9g %.9g, %.9g, %.9g" % vec)
+        node.AddValue(name, "%.9g, %.9g, %.9g, %.9g" % vec)
 
 def add_thing(thing, mu, node, exclude, add_funcs):
     for a in dir(thing):
