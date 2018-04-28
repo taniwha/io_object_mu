c8a6be73002c575ec8e06222a2e66dc2234e2ef5
Bill Currie <bill@taniwha.org>
2018-04-28 17:30:42 +0900

    Move AttachNode to its own file.

--- export_mu.py
+++ export_mu.py
@@ -40,6 +40,7 @@ from .shader import make_shader
 from . import properties
 from .cfgnode import ConfigNode, ConfigNodeError
 from .parser import parse_node
+from .attachnode import AttachNode
 
 def calcVolume(mesh):
     terms=[]
@@ -713,85 +714,3 @@ class VIEW3D_PT_tools_mu_export(bpy.types.Panel):
         #col = layout.column(align=True)
         layout.operator("export_object.ksp_mu_quick", text = "Export Mu Model");
         layout.operator("object.mu_volume", text = "Calc Mu Volume");
-
-def swapyz(vec):
-    return vec[0], vec[2], vec[1]
-
-class AttachNode:
-    node_types = ["stack", "attach"]
-    def __init__(self, obj, inv):
-        self.name = strip_nnn(obj.name)
-        self.parts = self.name.split("_", 2)
-        ind = self.parts[1] == "stack" and 2 or 1
-        self.id = "_".join(self.parts[ind:])
-        self.pos = swapyz((inv*obj.matrix_world.col[3])[:3])
-        self.dir = swapyz((inv*obj.matrix_world.col[2])[:3])
-        self.size = obj.muproperties.nodeSize
-        self.method = obj.muproperties.nodeMethod
-        self.crossfeed = obj.muproperties.nodeCrossfeed
-        self.rigid = obj.muproperties.nodeRigid
-    def __lt__(self, other):
-        return self.cmp(other) < 0
-    def __eq__(self, other):
-        return self.cmp(other) == 0
-    def __gt__(self, other):
-        return self.cmp(other) > 0
-    def cmp(self, other):
-        # parts[0] will always be "node"
-        if self.parts[1] == other.parts[1]:
-            x = len(self.parts) - len(other.parts)
-            if x != 0:
-                return x
-            if len(self.parts) < 3:
-                return 0
-            if self.parts[2] == other.parts[2]:
-                return 0
-            if self.parts[2] == "bottom":
-                return 1
-            if self.parts[2] == "top":
-                if other.parts[2] == "bottom":
-                    return -1
-                else:
-                    return 1
-            if other.parts[2] == "bottom":
-                return -1
-            if other.parts[2] == "top":
-                if self.parts[2] == "bottom":
-                    return 1
-                else:
-                    return -1
-            return self.parts[2] > other.parts[2] and 1 or -1
-        elif (self.parts[1] in self.node_types
-              and other.parts[1] in self.node_types):
-            return ord(other.parts[1][0]) - ord(self.parts[1][0])
-        else:
-            return self.parts[1] > other.parts[1] and 1 or -1
-    def __repr__(self):
-        return self.name + self.pos.__repr__() + self.dir.__repr__()
-    def methodval(self):
-        for i, enum in enumerate(properties.method_items):
-            if enum[0] == self.method:
-                return i
-        return 0
-    def cfgstring(self):
-        pos = tuple(map (lambda x: x * x > 1e-11 and x or 0, self.pos))
-        dir = tuple(map (lambda x: x * x > 1e-11 and x or 0, self.dir))
-        return "%g, %g, %g, %g, %g, %g, %d, %d, %d, %d" % (pos + dir + (self.size,self.methodval(), int(self.crossfeed), int(self.rigid)))
-    def cfgnode(self):
-        node = ConfigNode ()
-        node.AddValue ("name", self.id)
-        node.AddValue ("transform", self.name)
-        node.AddValue ("size", self.size)
-        node.AddValue ("method", self.method)
-        node.AddValue ("crossfeed", self.crossfeed)
-        node.AddValue ("rigid", self.rigid)
-        return node
-    def keep_transform(self):
-        return self.parts[1] not in ["attach"]
-    def save(self, cfg):
-        if self.parts[1] in ["attach"]:
-            # currently, KSP fails to check for attach NODEs so must use the
-            # old format
-            cfg.AddValue (self.name, self.cfgstring())
-        else:
-            cfg.AddNode("NODE", self.cfgnode())
