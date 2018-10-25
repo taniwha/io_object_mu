2572fdf62a9f811a79cbf9d8150f28a43adf6a28
Bill Currie <bill@taniwha.org>
2018-10-25 14:10:52 +0900

    Split out the config file export code.

--- export_mu/__init__.py
+++ export_mu/__init__.py
@@ -47,6 +47,7 @@ from .mesh import make_mesh
 from .collider import make_collider
 from .animation import collect_animations, find_path_root, make_animations
 from .material import make_material
+from .cfgfile import generate_cfg
 
 def make_transform(obj):
     transform = MuTransform()
@@ -200,74 +201,6 @@ def make_obj(mu, obj, special, path = ""):
         if child:
             muobj.children.append(child)
     return muobj
-
-def find_template(mu, filepath):
-    base = os.path.splitext(filepath)
-    cfg = base[0] + ".cfg"
-
-    cfgin = mu.name + ".cfg.in"
-    if cfgin in bpy.data.texts:
-        return cfg, ConfigNode.load(bpy.data.texts[cfgin].as_string())
-
-    cfgin = base[0] + ".cfg.in"
-    if os.path.isfile (cfgin):
-        try:
-            return cfg, ConfigNode.loadfile(cfgin)
-        except ConfigNodeError as e:
-            print("Error reading", cfgin, e.message)
-
-    return None, None
-
-def add_internal_node(node, internal):
-    # NOTE this assumes the internal is the direct child of the part's root
-    # also, it assumes the internal is correctly oriented relative to the part
-    # (FIXME?)
-    inode = node.AddNewNode('INTERNAL')
-    inode.AddValue("name", strip_nnn(internal.name))
-    if internal.location:
-        inode.AddValue("offset", vector_str(swapyz(internal.location)))
-    # not really a good idea IMHO, but it's there...
-    if internal.scale != Vector((1, 1, 1)):
-        inode.AddValue("scale", vector_str(swapyz(internal.scale)))
-
-def add_prop_node(node, prop):
-    # NOTE this assumes the prop is the direct child of the internal's root
-    pnode = node.AddNewNode('PROP')
-    pnode.AddValue("name", strip_nnn(prop.name))
-    pnode.AddValue("position", vector_str(swapyz(prop.location)))
-    pnode.AddValue("position", vector_str(swizzleq(prop.rotation_quaternion)))
-    pnode.AddValue("scale", vector_str(swapyz(prop.scale)))
-
-def generate_cfg(mu, filepath):
-    cfgfile, cfgnode = find_template(mu, filepath)
-    if not cfgnode:
-        return
-    ntype = mu.type
-    if ntype == 'NONE':
-        ntype = bpy.context.scene.musceneprops.modelType
-    node = cfgnode.GetNode(ntype)
-    if not node:
-        return
-    parse_node(mu, cfgnode)
-    if ntype == 'PART':
-        if mu.CoMOffset != None:
-            node.AddValue("CoMOffset", vector_str(swapyz(mu.CoMOffset)))
-        if mu.CoPOffset != None:
-            node.AddValue("CoPOffset", vector_str(swapyz(mu.CoPOffset)))
-        if mu.CoLOffset != None:
-            node.AddValue("CoLOffset", vector_str(swapyz(mu.CoLOffset)))
-        if mu.internal:
-            add_internal_node(node, mu.internal)
-        mu.nodes.sort()
-        for n in mu.nodes:
-            n.save(node)
-    elif ntype == 'INTERNAL':
-        for prop in mu.props:
-            add_prop_node(node, prop)
-    # nothing meaningful for PROP
-    of = open(cfgfile, "wt")
-    for n in cfgnode.nodes:
-        of.write(n[0] + " " + n[1].ToString())
 
 def add_internal(mu, obj):
     if not mu.internal:
