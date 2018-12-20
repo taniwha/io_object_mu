d5f1a789854e17ab16bc512a7ef321b53640738c
Bill Currie <bill@taniwha.org>
2018-12-20 10:04:53 +0900

    Convert \ to / before putting paths in the dictionary.
    Stupid microsoft.

--- import_craft/gamedata.py
+++ import_craft/gamedata.py
@@ -45,6 +45,7 @@ class GameData:
     def process_mu(self, path):
         gdpath = path[len(self.root):]
         directory, model = os.path.split(gdpath)
+        directory = directory.replace("\\", "/")
         if directory not in self.model_by_path:
             self.model_by_path[directory] = []
         self.model_by_path[directory].append(model[:-3])
