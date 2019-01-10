ff2b787044d0e3847e0cb4af38f5f8ce03da7165
Bill Currie <bill@taniwha.org>
2019-01-10 09:09:21 +0900

    Get the cleaned collider import working properly.

--- import_mu/import_mu.py
+++ import_mu/import_mu.py
@@ -63,13 +63,17 @@ def attach_material(mesh, renderer, mu):
         mumat = mu.materials[renderer.materials[0]]
         mesh.materials.append(mumat.material)
 
+def child_collider(mu, muobj, obj):
+    if mu.create_colliders and hasattr(muobj, "collider"):
+        if obj.data:
+            cobj = create_collider(mu, muobj)
+            set_transform(cobj, None)
+            mu.collection.objects.link(cobj)
+            cobj.parent = obj
+
 def create_object(mu, muobj, parent):
     obj = None
     mesh = None
-    if (not mu.create_colliders
-        and (hasattr(muobj, "shared_mesh")
-             and not hasattr(muobj, "renderer"))):
-        return None
     name = muobj.transform.name
     xform = None if hasattr(muobj, "bone") else muobj.transform
     if hasattr(muobj, "shared_mesh") and hasattr(muobj, "renderer"):
@@ -78,6 +82,7 @@ def create_object(mu, muobj, parent):
             poly.use_smooth = True
         obj = create_data_object(name, mesh, xform)
         attach_material(mesh, muobj.renderer, mu)
+        child_collider(mu, muobj, obj)
     elif hasattr(muobj, "skinned_mesh_renderer"):
         smr = muobj.skinned_mesh_renderer
         mesh = create_mesh(mu, smr.mesh, name)
@@ -87,6 +92,7 @@ def create_object(mu, muobj, parent):
         create_vertex_groups(obj, smr.bones, smr.mesh.boneWeights)
         create_armature_modifier(obj, mu)
         attach_material(mesh, smr, mu)
+        child_collider(mu, muobj, obj)
     if not obj:
         data = None
         if hasattr(muobj, "light"):
@@ -114,6 +120,7 @@ def create_object(mu, muobj, parent):
     else:
         if not obj:
             if mu.create_colliders and hasattr(muobj, "collider"):
+                print(muobj.transform.name)
                 obj = create_collider(mu, muobj)
                 set_transform(obj, xform)
             else:
@@ -142,12 +149,6 @@ def create_object(mu, muobj, parent):
         if hasattr(muobj, "tag_and_layer"):
             obj.muproperties.tag = muobj.tag_and_layer.tag
             obj.muproperties.layer = muobj.tag_and_layer.layer
-        if mu.create_colliders and hasattr(muobj, "collider"):
-            if obj.data:
-                cobj = create_collider(mu, muobj)
-                set_transform(cobj, None)
-                mu.collection.objects.link(cobj)
-                cobj.parent = obj
         muobj.bobj = obj
     for child in muobj.children:
         create_object(mu, child, obj)
