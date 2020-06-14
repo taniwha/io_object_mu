359c986350d72bc200e5d191c5fd90fa6c5d0478
Bill Currie <bill@taniwha.org>
2020-06-14 20:59:40 +0900

    Revert "Add a tiny bit of epsilon to plane checking"
    This reverts commit 61cce9626968736137f7d8d486eeeca77facf0de.
    
    Epsilon really is not the answer. It just moves the problem around :(

--- quickhull/triangle.py
+++ quickhull/triangle.py
@@ -34,6 +34,7 @@ except ImportError:
     from edge import Edge
 
 id = 0
+epsilon = 1e-5
 
 class Triangle:
     def __init__(self, mesh, a, b, c):
@@ -83,7 +84,7 @@ class Triangle:
 
     def is_dup(self, point):
         p = self.mesh.verts[point]
-        e = 1e-6    # 1mm (due to square)
+        e = 1e-6
         d = sub(p, self.a)
         h = dot(d, self.n)
         if h * h > e:
@@ -109,7 +110,7 @@ class Triangle:
         # triangle's plane (not a propblem, but subotptimal) and the height
         # is needed anyway as in the end, the highest point is desired.
         d = self.dist(point)
-        if d <= 1e-6: # 1um
+        if d <= 0:
             return False
         elif d > self.height:
             self.height = d
